b86d9252779359b1d46221f8c0f6d7d6bf273ce6
Charles Gravolet <charles_gravolet@comcast.com>
2022-12-03 12:50:52 -0500

    Day 3, Part 2

--- cmd/dec03/main.go
+++ cmd/dec03/main.go
@@ -9,6 +9,27 @@ import (
 	"github.comcast.com/cgravo558/advent2022/pkg/utility"
 )
 
+type Compartment string
+type Item rune
+
+func (lhs Compartment) GetCommonItems(rhs Compartment) []Item {
+	commonItems := make([]Item, 0)
+	for _, el := range lhs {
+		if strings.IndexRune(string(rhs), el) > -1 && strings.IndexRune(string(commonItems), el) == -1 {
+			commonItems = append(commonItems, Item(el))
+		}
+	}
+	return commonItems
+}
+
+func (i Item) Priority() int {
+	if int(i) > 96 {
+		return int(i) - 96
+	} else {
+		return int(i) - 64 + 26
+	}
+}
+
 func main() {
 	part1()
 	part2()
@@ -20,25 +41,11 @@ func part1() {
 		if len(s) == 0 {
 			return
 		}
-
-		// Split rucksack into 2 compartments
 		mid := len(s) / 2
-		c1 := s[:mid]
-		c2 := s[mid:]
+		commonItems := Compartment(s[:mid]).GetCommonItems(Compartment(s[mid:]))
 
-		// Find priority of common item in compartments and add it to sum
-		var commonItems string
-
-		for _, el := range c1 {
-			if strings.IndexRune(c2, el) > -1 && strings.IndexRune(commonItems, el) == -1 {
-				commonItems += string(el)
-
-				if int(el) > 96 {
-					sum += int(el) - 96
-				} else {
-					sum += int(el) - 64 + 26
-				}
-			}
+		for _, i := range commonItems {
+			sum += i.Priority()
 		}
 	})
 
@@ -50,5 +57,37 @@ func part1() {
 
 func part2() {
 	var sum int
+
+	groups := make([][]Compartment, 0)
+
+	err := utility.ForEachLineInFile(os.Args[1], func(s string) {
+		index := len(groups) - 1
+
+		if index < 0 || len(groups[index]) == 3 {
+			index += 1
+			groups = append(groups, []Compartment{})
+		}
+		groups[index] = append(groups[index], Compartment(s))
+	})
+
+	for _, g := range groups {
+
+		if len(g) <= 0 {
+			continue
+		}
+		commonItems := []Item(g[0])
+
+		for r := 1; r < len(g); r += 1 {
+			commonItems = g[r].GetCommonItems(Compartment(commonItems))
+		}
+
+		for _, i := range commonItems {
+			sum += i.Priority()
+		}
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("Priority sum of badges (part 2): %d\n", sum)
 }
