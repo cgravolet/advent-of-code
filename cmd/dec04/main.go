f0b8d80d2a2798ba2779011e5c6f9ec67d74424f
Charles Gravolet <charles_gravolet@comcast.com>
2022-12-04 17:23:42 -0500

    Cleanup

--- cmd/dec04/main.go
+++ cmd/dec04/main.go
@@ -4,61 +4,27 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.comcast.com/cgravo558/advent2022/pkg/utility"
 )
 
-type Section struct {
+type SectionRange struct {
 	Start int
 	End   int
 }
 
-func MakeSection(s string) (Section, error) {
-	parameters := strings.Split(s, "-")
-	section := Section{}
-
-	if len(parameters) != 2 {
-		return section, fmt.Errorf("Incorrect format")
-	}
-	start, err := strconv.Atoi(parameters[0])
-
-	if err != nil {
-		return section, err
-	}
-	end, err := strconv.Atoi(parameters[1])
-
-	if err != nil {
-		return section, err
-	}
-	section.Start = start
-	section.End = end
-	return Section{start, end}, nil
-}
-
-func (s1 Section) IsContainedBySection(s2 Section) bool {
+func (s1 SectionRange) IsContainedBySection(s2 SectionRange) bool {
 	return s1.Start >= s2.Start && s1.End <= s2.End
 }
 
-func (s1 Section) IsOverlappedBySection(s2 Section) bool {
+func (s1 SectionRange) IsOverlappedBySection(s2 SectionRange) bool {
 	return !(s1.Start > s2.End || s1.End < s2.Start)
 }
 
-func makeSectionPairFromString(s string) ([]Section, error) {
-	sections := make([]Section, 0)
-	for _, e := range strings.Split(s, ",") {
-		sec, err := MakeSection(e)
-
-		if err != nil {
-			return nil, err
-		}
-		sections = append(sections, sec)
-	}
-	if len(sections) != 2 {
-		return nil, fmt.Errorf("Expected 2 sections, got %d instead.", len(sections))
-	}
-	return sections, nil
+func makeSectionPairFromString(s string) (SectionRange, SectionRange, error) {
+	var lhs, rhs SectionRange
+	_, err := fmt.Sscanf(s, "%d-%d,%d-%d", &lhs.Start, &lhs.End, &rhs.Start, &rhs.End)
+	return lhs, rhs, err
 }
 
 func main() {
@@ -66,17 +32,17 @@ func main() {
 	var overlapped int
 
 	err := utility.ForEachLineInFile(os.Args[1], func(s string) {
-		sec, err := makeSectionPairFromString(s)
+		lhs, rhs, err := makeSectionPairFromString(s)
 
 		if err != nil {
 			return
 		}
 
-		if sec[0].IsContainedBySection(sec[1]) || sec[1].IsContainedBySection(sec[0]) {
+		if lhs.IsContainedBySection(rhs) || rhs.IsContainedBySection(lhs) {
 			contained += 1
 		}
 
-		if sec[0].IsOverlappedBySection(sec[1]) {
+		if lhs.IsOverlappedBySection(rhs) {
 			overlapped += 1
 		}
 	})
