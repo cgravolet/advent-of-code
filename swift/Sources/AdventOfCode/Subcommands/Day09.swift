9d4cd95b61846665cca310b416a0462174135190
Charles Gravolet <charles_gravolet@comcast.com>
2022-12-09 09:24:54 -0500

    Cleanup

--- swift/Sources/AdventOfCode/Subcommands/Day09.swift
+++ swift/Sources/AdventOfCode/Subcommands/Day09.swift
@@ -1,64 +1,66 @@
 import ArgumentParser
 import Foundation
 
-enum Direction: Equatable {
-    case up(Int)
-    case right(Int)
-    case down(Int)
-    case left(Int)
+struct Day09: ParsableCommand {
+    static let configuration = CommandConfiguration(abstract: "Advent of Code - 2022 December 9", version: "1.0.0")
 
-    init?(_ rawValue: String) {
-        let components = rawValue.components(separatedBy: .whitespaces)
-        guard components.count == 2, let count = Int(components[1]) else { return nil }
-        switch components[0] {
-        case "U": self = .up(count)
-        case "R": self = .right(count)
-        case "D": self = .down(count)
-        case "L": self = .left(count)
-        default: return nil
+    // MARK - Data structures
+
+    enum Direction: Equatable {
+        case up(Int), right(Int), down(Int), left(Int)
+
+        init?(_ rawValue: String) {
+            let components = rawValue.components(separatedBy: .whitespaces)
+            guard components.count == 2, let count = Int(components[1]) else { return nil }
+            switch components[0] {
+            case "U": self = .up(count)
+            case "R": self = .right(count)
+            case "D": self = .down(count)
+            case "L": self = .left(count)
+            default: return nil
+            }
+        }
+
+        var coord: IntPoint {
+            switch self {
+            case .up: return IntPoint(0, -1)
+            case .right: return IntPoint(1, 0)
+            case .down: return IntPoint(0, 1)
+            case .left: return IntPoint(-1, 0)
+            }
+        }
+
+        var count: Int {
+            switch self {
+            case .up(let count): return count
+            case .right(let count): return count
+            case .down(let count): return count
+            case .left(let count): return count
+            }
         }
     }
 
-    var coord: IntPoint {
-        switch self {
-        case .up: return IntPoint(0, -1)
-        case .right: return IntPoint(1, 0)
-        case .down: return IntPoint(0, 1)
-        case .left: return IntPoint(-1, 0)
+    struct IntPoint: Hashable {
+        let x: Int
+        let y: Int
+
+        init(_ x: Int, _ y: Int) {
+            self.x = x
+            self.y = y
         }
-    }
 
-    var count: Int {
-        switch self {
-        case .up(let count): return count
-        case .right(let count): return count
-        case .down(let count): return count
-        case .left(let count): return count
+        func isTouching(_ point: IntPoint) -> Bool {
+            abs(x - point.x) <= 1 && abs(y - point.y) <= 1
         }
-    }
-}
 
-struct IntPoint: Hashable {
-    let x: Int
-    let y: Int
+        func move(_ direction: Direction) -> IntPoint {
+            let moveCoord = direction.coord
+            return IntPoint(x + moveCoord.x, y + moveCoord.y)
+        }
 
-    init(_ x: Int, _ y: Int) {
-        self.x = x
-        self.y = y
-    }
-
-    func isTouching(_ point: IntPoint) -> Bool {
-        abs(x - point.x) <= 1 && abs(y - point.y) <= 1
-    }
-
-    func move(_ direction: Direction) -> IntPoint {
-        let moveCoord = direction.coord
-        return IntPoint(x + moveCoord.x, y + moveCoord.y)
-    }
-
-    func reposition(near h: IntPoint) -> IntPoint {
-        var t = self
-        if !t.isTouching(h) {
+        func reposition(near h: IntPoint) -> IntPoint {
+            guard !isTouching(h) else { return self }
+            var t = self
             let xdiff = h.x - t.x
             let ydiff = h.y - t.y
             let movex = abs(xdiff) > 1 || (abs(xdiff) > 0 && abs(ydiff) > 1)
@@ -67,13 +69,9 @@ struct IntPoint: Hashable {
                 movex ? t.x + (xdiff < 0 ? -1 : 1) : t.x,
                 movey ? t.y + (ydiff < 0 ? -1 : 1) : t.y
             )
+            return t
         }
-        return t
     }
-}
-
-struct Day09: ParsableCommand {
-    static let configuration = CommandConfiguration(abstract: "Advent of Code - 2022 December 9", version: "1.0.0")
 
     // MARK: - Options
 
@@ -99,7 +97,7 @@ struct Day09: ParsableCommand {
     func solve(instructions: [Direction], ropeSize: Int) -> Int {
         guard ropeSize > 0 else { return 0 }
         var rope = [IntPoint](repeating: IntPoint(500, 500), count: ropeSize)
-        var visited = [IntPoint: Bool]()
+        var visited = Set<IntPoint>()
 
         for direction in instructions {
             for _ in 0 ..< direction.count {
@@ -107,7 +105,7 @@ struct Day09: ParsableCommand {
                 for i in 1 ..< rope.count {
                     rope[i] = rope[i].reposition(near: rope[i-1])
                 }
-                visited[rope[rope.count-1]] = true
+                visited.insert(rope[rope.count-1])
             }
         }
         return visited.count
--- swift/Tests/AdventOfCodeTests/Day09Tests.swift
+++ swift/Tests/AdventOfCodeTests/Day09Tests.swift
@@ -3,7 +3,7 @@ import XCTest
 import class Foundation.Bundle
 
 final class Day09Tests: XCTestCase {
-    private var sampleInput = """
+    private var smallSample = """
     R 4
     U 4
     L 3
@@ -12,34 +12,44 @@ final class Day09Tests: XCTestCase {
     D 1
     L 5
     R 2
+    """
 
+    private var largeSample = """
+    R 5
+    U 8
+    L 8
+    D 3
+    R 17
+    D 10
+    L 25
+    U 20
     """
 
     func testMakeInstructions() throws {
-        let want: [Direction] = [.right(4), .up(4), .left(3), .down(1), .right(4), .down(1), .left(5), .right(2)]
+        let want: [Day09.Direction] = [.right(4), .up(4), .left(3), .down(1), .right(4), .down(1), .left(5), .right(2)]
         let sut = Day09()
-        let got = sut.makeInstructions(fromString: sampleInput)
+        let got = sut.makeInstructions(fromString: smallSample)
         XCTAssertEqual(got, want)
     }
 
     func testIsTouching() throws {
-        let tests: [(lhs: IntPoint, rhs: IntPoint, want: Bool)] = [
-            (IntPoint(100, 100), IntPoint(100, 100), true),
-            (IntPoint(100, 100), IntPoint(99, 100), true),
-            (IntPoint(100, 100), IntPoint(98, 100), false),
-            (IntPoint(100, 100), IntPoint(101, 100), true),
-            (IntPoint(100, 100), IntPoint(102, 100), false),
-            (IntPoint(100, 100), IntPoint(100, 99), true),
-            (IntPoint(100, 100), IntPoint(100, 98), false),
-            (IntPoint(100, 100), IntPoint(100, 101), true),
-            (IntPoint(100, 100), IntPoint(100, 102), false),
-            (IntPoint(100, 100), IntPoint(99, 99), true),
-            (IntPoint(100, 100), IntPoint(98, 99), false),
-            (IntPoint(100, 100), IntPoint(99, 98), false),
-            (IntPoint(100, 100), IntPoint(101, 99), true),
-            (IntPoint(100, 100), IntPoint(101, 98), false),
-            (IntPoint(100, 100), IntPoint(101, 101), true),
-            (IntPoint(100, 100), IntPoint(101, 102), false)
+        let tests: [(lhs: Day09.IntPoint, rhs: Day09.IntPoint, want: Bool)] = [
+            (.init(100, 100), .init(100, 100), true),
+            (.init(100, 100), .init(99, 100), true),
+            (.init(100, 100), .init(98, 100), false),
+            (.init(100, 100), .init(101, 100), true),
+            (.init(100, 100), .init(102, 100), false),
+            (.init(100, 100), .init(100, 99), true),
+            (.init(100, 100), .init(100, 98), false),
+            (.init(100, 100), .init(100, 101), true),
+            (.init(100, 100), .init(100, 102), false),
+            (.init(100, 100), .init(99, 99), true),
+            (.init(100, 100), .init(98, 99), false),
+            (.init(100, 100), .init(99, 98), false),
+            (.init(100, 100), .init(101, 99), true),
+            (.init(100, 100), .init(101, 98), false),
+            (.init(100, 100), .init(101, 101), true),
+            (.init(100, 100), .init(101, 102), false)
         ]
 
         for test in tests {
@@ -51,8 +61,16 @@ final class Day09Tests: XCTestCase {
     func testSolve1() throws {
         let want = 13
         let sut = Day09()
-        let instructions = sut.makeInstructions(fromString: sampleInput)
+        let instructions = sut.makeInstructions(fromString: smallSample)
         let got = sut.solve(instructions: instructions, ropeSize: 2)
+        XCTAssertEqual(got, want)
+    }
+
+    func testSolve2() throws {
+        let want = 36
+        let sut = Day09()
+        let instructions = sut.makeInstructions(fromString: largeSample)
+        let got = sut.solve(instructions: instructions, ropeSize: 10)
         XCTAssertEqual(got, want)
     }
 }
