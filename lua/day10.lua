bdd7be0cc1f698906de7e910dd7ad935ccdf03e5
Charles Gravolet <charles_gravolet@comcast.com>
2022-12-10 14:43:40 -0500

    Setup lua project

---
+++ lua/day10.lua
@@ -0,0 +1,60 @@
+local utility = require("utility")
+
+Day10 = {
+    path = "../input/day10.txt"
+}
+
+function Day10:run()
+    local f = assert(io.open(self.path, "r"))
+    local s = f:read("*all")
+    f:close()
+    local instructions = self.make_instructions(s)
+    local part1, part2 = self.solve(instructions)
+    print("Part 1:\n" .. part1 .. "\n")
+    print("Part 2:\n" .. part2 .. "\n")
+end
+
+function Day10.make_instructions(s)
+    local instructions = {}
+
+    for line in s:gmatch("([^\n]*)\n?") do
+        local i, j = string.find(line, "-?%d+")
+
+        if i then
+            local val = line:sub(i, j)
+            table.insert(instructions, {cycles = 2, value = tonumber(val)})
+        else
+            table.insert(instructions, {cycles = 1, value = 0})
+        end
+    end
+    return instructions
+end
+
+function Day10.solve(ins)
+    local cycle, pixels, pos, x, sum = 1, "", 0, 1, 0
+
+    while #ins > 0 do
+        if (cycle - 20) % 40 == 0 then sum = sum + x * cycle end
+
+        -- Draw the pixel
+        local pixel = "."
+        if pos >= x - 1 and pos <= x + 1 then pixel = "#" end
+        pixels, pos = pixels .. pixel, pos + 1
+
+        -- Reset position if this is end of the CRT's draw distance
+        if pos % 40 == 0 then
+            pixels, pos = pixels .. "\n", 0
+        end
+
+        -- Apply instruction to move the register
+        ins[1].cycles = ins[1].cycles - 1
+        if ins[1].cycles <= 0 then
+            x = x + ins[1].value
+            table.remove(ins, 1)
+        end
+        cycle = cycle + 1
+    end
+    return sum, pixels
+end
+
+Day10:run()
---
+++ lua/utility.lua
@@ -0,0 +1,43 @@
+local utility = {}
+
+function utility.file_exists(file)
+    local f = io.open(file, "rb")
+    if f then f:close() end
+    return f ~= nil
+end
+
+function utility.lines_from(file)
+    if not utility.file_exists(file) then return {} end
+    local lines = {}
+    for line in io.lines(file) do
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+function utility.tprint(tbl, indent)
+    if not indent then indent = 0 end
+    local toprint = string.rep(" ", indent) .. "{\r\n"
+    indent = indent + 2
+    for k, v in pairs(tbl) do
+        toprint = toprint .. string.rep(" ", indent)
+        if (type(k) == "number") then
+            toprint = toprint .. "[" .. k .. "] = "
+        elseif (type(k) == "string") then
+            toprint = toprint  .. k ..  "= "
+        end
+        if (type(v) == "number") then
+            toprint = toprint .. v .. ",\r\n"
+        elseif (type(v) == "string") then
+            toprint = toprint .. "\"" .. v .. "\",\r\n"
+        elseif (type(v) == "table") then
+            toprint = toprint .. utility.tprint(v, indent + 2) .. ",\r\n"
+        else
+            toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+        end
+    end
+    toprint = toprint .. string.rep(" ", indent-2) .. "}"
+    return toprint
+end
+
+return utility
