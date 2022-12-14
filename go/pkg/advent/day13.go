1c3f6644ace639901a5367676001afe5a3326544
Charles Gravolet <charles_gravolet@comcast.com>
2022-12-14 08:33:49 -0500

    Day 13 cleanup

--- go/pkg/advent/day13.go
+++ go/pkg/advent/day13.go
@@ -25,91 +25,6 @@ func (a *AdventOfCode2022) Day13(input string) {
 	fmt.Printf("Part 2: %d\n", part2)
 }
 
-func compareGroup(l, r []any) int {
-	fmt.Printf("\t- Compare %v vs %v\n", l, r)
-
-	if len(l) == 0 && len(r) > 0 {
-		fmt.Printf("\t- Left side ran out of items, so inputs are in the right order\n")
-		return 1 // Left side out of items, ordered correctly
-	} else if len(l) > 0 && len(r) == 0 {
-		fmt.Printf("\t- Right side ran out of items, so inputs are not in the right order\n")
-		return -1 // Right side out of items, not ordered correctly
-	}
-
-	for len(l) > 0 {
-
-		if len(r) == 0 {
-			fmt.Printf("\t- Right side ran out of items, so inputs are not in the right order\n")
-			return -1 // Right side out of items, not ordered correctly
-		}
-		lhs, rhs := l[0], r[0]
-		l, r = l[1:], r[1:]
-		lnum, lnumok := lhs.(float64)
-		rnum, rnumok := rhs.(float64)
-
-		if lnumok && rnumok {
-			fmt.Printf("\t\t- Compare %d vs %d\n", int(lnum), int(rnum))
-
-			if int(lnum) > int(rnum) {
-				fmt.Printf("\t\t\t- Right side is smaller, so inputs are not in the right order\n")
-				return -1 // Left side greater than right, not ordered correctly
-			} else if int(lnum) < int(rnum) {
-				fmt.Printf("\t\t\t- Left side is smaller, so inputs are in the right order\n")
-				return 1 // Left side greater than right, not ordered correctly
-			}
-		} else {
-			larr, larrok := lhs.([]any)
-			rarr, rarrok := rhs.([]any)
-
-			if larrok && rnumok {
-				fmt.Printf("\t- Mixed types; convert right to [%f] and retry comparison\n", rnum)
-				comparison := compareGroup(larr, []any{rnum})
-				if comparison != 0 {
-					return comparison
-				}
-			} else if lnumok && rarrok {
-				fmt.Printf("\t- Mixed types; convert left to [%f] and retry comparison\n", lnum)
-				comparison := compareGroup([]any{lnum}, rarr)
-				if comparison != 0 {
-					return comparison
-				}
-			} else if larrok && rarrok {
-				comparison := compareGroup(larr, rarr)
-				if comparison != 0 {
-					return comparison
-				}
-			} else {
-				panic(fmt.Errorf("Encountered unrecognized type %v", lhs))
-			}
-		}
-	}
-
-	if len(r) > 0 {
-		fmt.Printf("\t- Left side ran out of items, so inputs are in the right order\n")
-		return 1
-	}
-	return 0
-}
-
-func comparePackets(l, r string) (int, error) {
-	lqueue, lerr := decodePacket(l)
-	rqueue, rerr := decodePacket(r)
-
-	if lerr != nil {
-		return -1, lerr
-	} else if rerr != nil {
-		return -1, rerr
-	}
-	fmt.Printf("Comparing %v vs %v\n", lqueue, rqueue)
-	return compareGroup(lqueue, rqueue), nil
-}
-
-func decodePacket(packet string) ([]any, error) {
-	var result []any
-	err := json.Unmarshal([]byte(packet), &result)
-	return result, err
-}
-
 func day13part1(input string) (int, error) {
 	result := 0
 
@@ -118,7 +33,6 @@ func day13part1(input string) (int, error) {
 
 		if len(packets) >= 2 {
 			comparison, err := comparePackets(packets[0], packets[1])
-			fmt.Printf("\tResult %v\n", comparison)
 
 			if err != nil {
 				return result, err
@@ -145,8 +59,7 @@ func day13part2(input string) (int, error) {
 
 	dividerPacket1 := []any{[]any{2.0}}
 	dividerPacket2 := []any{[]any{6.0}}
-	packets = append(packets, dividerPacket1)
-	packets = append(packets, dividerPacket2)
+	packets = append(packets, dividerPacket1, dividerPacket2)
 
 	sort.Slice(packets, func(l, r int) bool {
 		comparison := compareGroup(packets[l], packets[r])
@@ -164,4 +77,76 @@ func day13part2(input string) (int, error) {
 		}
 	}
 	return key, nil
+}
+
+// Internal methods
+
+func compareGroup(l, r []any) int {
+	if len(l) == 0 && len(r) > 0 {
+		return 1
+	} else if len(l) > 0 && len(r) == 0 {
+		return -1
+	}
+
+	for len(l) > 0 {
+
+		if len(r) == 0 {
+			return -1
+		}
+		lhs, rhs := l[0], r[0]
+		l, r = l[1:], r[1:]
+		lnum, lnumok := lhs.(float64)
+		rnum, rnumok := rhs.(float64)
+
+		if lnumok && rnumok {
+			if int(lnum) > int(rnum) {
+				return -1
+			} else if int(lnum) < int(rnum) {
+				return 1
+			} else {
+				continue
+			}
+		}
+		larr, larrok := lhs.([]any)
+		rarr, rarrok := rhs.([]any)
+
+		if larrok && rnumok {
+			if comparison := compareGroup(larr, []any{rnum}); comparison != 0 {
+				return comparison
+			}
+		} else if lnumok && rarrok {
+			if comparison := compareGroup([]any{lnum}, rarr); comparison != 0 {
+				return comparison
+			}
+		} else if larrok && rarrok {
+			if comparison := compareGroup(larr, rarr); comparison != 0 {
+				return comparison
+			}
+		} else {
+			panic(fmt.Errorf("Encountered unrecognized type %v", lhs))
+		}
+	}
+
+	if len(r) > 0 {
+		return 1
+	}
+	return 0
+}
+
+func comparePackets(l, r string) (int, error) {
+	lqueue, lerr := decodePacket(l)
+	rqueue, rerr := decodePacket(r)
+
+	if lerr != nil {
+		return -1, lerr
+	} else if rerr != nil {
+		return -1, rerr
+	}
+	return compareGroup(lqueue, rqueue), nil
+}
+
+func decodePacket(packet string) ([]any, error) {
+	var result []any
+	err := json.Unmarshal([]byte(packet), &result)
+	return result, err
 }
