e8b6d11ce8e9b2d550fd1fc323d76bf6ad8d2156
cgravolet <cgravolet@gmail.com>
2022-12-26 13:01:30 -0500

    Day 25, part 1

---
+++ go/pkg/advent/day25.go
@@ -0,0 +1,58 @@
+package advent
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cgravolet/adventofcode2022/pkg/utility"
+)
+
+func (a *AdventOfCode2022) Day25(input string) {
+	part1a, part1b := solveDay25Part1(input)
+	fmt.Printf("Part 1: %s (%d)\n", part1b, part1a)
+}
+
+// Data structures
+
+type SnafuNumber string
+
+func (sn SnafuNumber) IntValue() int {
+	var sum int
+	pow := 1
+	for i := len(sn) - 1; i >= 0; i-- {
+		var num int
+		switch sn[i] {
+		case '-':
+			num = -1
+		case '=':
+			num = -2
+		default:
+			num, _ = strconv.Atoi(string(sn[i]))
+		}
+		sum += pow * num
+		pow *= 5
+	}
+	return sum
+}
+
+func NewSnafuNumber(input int) SnafuNumber {
+	var snafu string
+	sum := input
+
+	for sum > 0 {
+		snafu = string("=-012"[(sum+2)%5]) + snafu
+		sum = (sum + 2) / 5
+	}
+	return SnafuNumber(snafu)
+}
+
+// Solve
+
+func solveDay25Part1(input string) (int, SnafuNumber) {
+	var sum int
+	utility.ForEachLineInReader(strings.NewReader(input), func(s string) {
+		sum += SnafuNumber(s).IntValue()
+	})
+	return sum, NewSnafuNumber(sum)
+}
---
+++ go/pkg/advent/day25_test.go
@@ -0,0 +1,125 @@
+package advent
+
+import (
+	"reflect"
+	"testing"
+)
+
+var sample25 = `1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122
+`
+
+func TestSnafuIntValue(t *testing.T) {
+	type test struct {
+		want  int
+		input SnafuNumber
+	}
+	tests := []test{
+		{1, "1"},
+		{2, "2"},
+		{3, "1="},
+		{4, "1-"},
+		{5, "10"},
+		{6, "11"},
+		{7, "12"},
+		{8, "2="},
+		{9, "2-"},
+		{10, "20"},
+		{15, "1=0"},
+		{20, "1-0"},
+		{2022, "1=11-2"},
+		{12345, "1-0---0"},
+		{314159265, "1121-1110-1=0"},
+		{1747, "1=-0-2"},
+		{906, "12111"},
+		{198, "2=0="},
+		{11, "21"},
+		{201, "2=01"},
+		{31, "111"},
+		{1257, "20012"},
+		{32, "112"},
+		{353, "1=-1="},
+		{107, "1-12"},
+		{7, "12"},
+		{3, "1="},
+		{37, "122"},
+	}
+
+	for _, test := range tests {
+		got := test.input.IntValue()
+
+		if !reflect.DeepEqual(test.want, got) {
+			t.Errorf("expected: %v, got: %v", test.want, got)
+		}
+	}
+}
+
+func TestNewSnafuNumber(t *testing.T) {
+	type test struct {
+		input int
+		want  SnafuNumber
+	}
+	tests := []test{
+		{1, "1"},
+		{2, "2"},
+		{3, "1="},
+		{4, "1-"},
+		{5, "10"},
+		{6, "11"},
+		{7, "12"},
+		{8, "2="},
+		{9, "2-"},
+		{10, "20"},
+		{15, "1=0"},
+		{20, "1-0"},
+		{2022, "1=11-2"},
+		{12345, "1-0---0"},
+		{314159265, "1121-1110-1=0"},
+		{1747, "1=-0-2"},
+		{906, "12111"},
+		{198, "2=0="},
+		{11, "21"},
+		{201, "2=01"},
+		{31, "111"},
+		{1257, "20012"},
+		{32, "112"},
+		{353, "1=-1="},
+		{107, "1-12"},
+		{7, "12"},
+		{3, "1="},
+		{37, "122"},
+	}
+
+	for _, test := range tests {
+		got := NewSnafuNumber(test.input)
+
+		if !reflect.DeepEqual(test.want, got) {
+			t.Errorf("expected: %v, got: %v", test.want, got)
+		}
+	}
+}
+
+func TestSolveDay25Part1(t *testing.T) {
+	wantnum := 4890
+	wantstr := SnafuNumber("2=-1=0")
+	gotnum, gotstr := solveDay25Part1(sample25)
+
+	if !reflect.DeepEqual(wantnum, gotnum) {
+		t.Errorf("expected: %v, got: %v", wantnum, gotnum)
+	}
+
+	if !reflect.DeepEqual(wantstr, gotstr) {
+		t.Errorf("expected: %v, got: %v", wantstr, gotstr)
+	}
+}
