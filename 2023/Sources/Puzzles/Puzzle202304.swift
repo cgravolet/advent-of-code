093c7cfb4d8c39012af6ba111af1d118abfecaba
cgravolet <cgravolet@gmail.com>
2023-12-04 07:46:46 -0500

    Solve - Day 4, part 1

--- 2023/Sources/Puzzles/Puzzle202304.swift
+++ 2023/Sources/Puzzles/Puzzle202304.swift
@@ -8,7 +8,10 @@ struct Puzzle202304: Puzzle {
   // MARK: - Public methods
 
   func solve1() throws -> Any {
-    -1
+    input
+      .components(separatedBy: .newlines)
+      .map(points)
+      .reduce(0, +)
   }
 
   func solve2() throws -> Any {
@@ -16,4 +19,29 @@ struct Puzzle202304: Puzzle {
   }
 
   // MARK: - Private methods
+
+  func points(from input: String) -> Int {
+    guard let sets = input.split(separator: ": ").last?.split(separator: " | ").map(String.init).map(parseNumberSet),
+      sets.count == 2
+    else { return 0 }
+    let winningNumbers = sets[0].intersection(sets[1])
+    let winningCount = winningNumbers.count
+
+    if winningCount > 0 {
+      let value: UInt = 1 << (winningCount - 1)
+      return Int(value)
+    }
+    return 0
+  }
+
+  /// Returns a Set of integers from a string (i.e. "41 48 83 86 17")
+  func parseNumberSet(_ input: String) -> Set<Int> {
+    var numberSet = Set<Int>()
+    for match in input.matches(of: /(\d+)/) {
+      if let value = Int(match.output.1) {
+        numberSet.insert(value)
+      }
+    }
+    return numberSet
+  }
 }
