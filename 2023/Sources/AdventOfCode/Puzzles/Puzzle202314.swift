37ea1e2997b9e89694a52d99c509e79b70747e27
cgravolet <cgravolet@gmail.com>
2023-12-14 13:18:15 -0500

    Solve - Day 14, part 1

--- 2023/Sources/AdventOfCode/Puzzles/Puzzle202314.swift
+++ 2023/Sources/AdventOfCode/Puzzles/Puzzle202314.swift
@@ -8,7 +8,7 @@ struct Puzzle202314: Puzzle {
   // MARK: - Public methods
 
   func solve1() throws -> Any {
-    return -1
+    calculateLoad(from: tiltMap(Map2D(input)))
   }
 
   func solve2() throws -> Any {
@@ -16,4 +16,28 @@ struct Puzzle202314: Puzzle {
   }
 
   // MARK: - Private methods
+
+  private func calculateLoad(from map: Map2D) -> Int {
+    map.coords(matching: /O/).reduce(0, { $0 + (map.maxY + 1 - $1.y) })
+  }
+
+  private func tiltMap(_ map: Map2D) -> Map2D {
+    var mutableMap = map
+    let roundRocks = map.coords(matching: /O/).sorted(by: { $0.y < $1.y })
+
+    for rock in roundRocks {
+      var newY = rock.y
+
+      for y in (0..<rock.y).reversed() {
+        if mutableMap.value(at: Coord2D(rock.x, y)) == nil {
+          newY = y
+        } else {
+          break
+        }
+      }
+      mutableMap.set(value: nil, at: rock)
+      mutableMap.set(value: "O", at: Coord2D(rock.x, newY))
+    }
+    return mutableMap
+  }
 }
--- 2023/Tests/AdventOfCodeTests/Puzzle202314Tests.swift
+++ 2023/Tests/AdventOfCodeTests/Puzzle202314Tests.swift
@@ -4,11 +4,21 @@ import XCTest
 
 final class Puzzle202314Tests: XCTestCase {
   let input = """
+    O....#....
+    O.OO#....#
+    .....##...
+    OO.#O....O
+    .O.....O#.
+    O.#..O.#.#
+    ..O..#O..O
+    .......O..
+    #....###..
+    #OO..#....
     """
 
   func testSolve1() throws {
     let sut = Puzzle202314(input: input)
-    XCTAssertEqual(try sut.solve1() as? Int, -1)
+    XCTAssertEqual(try sut.solve1() as? Int, 136)
   }
 
   func testSolve2() throws {
