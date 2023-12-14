d9647b131795b7bd98efecc4a05a5bed5a6002e9
cgravolet <cgravolet@gmail.com>
2023-12-14 10:37:55 -0500

    Solve - Day 13, part 1

--- 2023/Sources/AdventOfCode/Puzzles/Puzzle202313.swift
+++ 2023/Sources/AdventOfCode/Puzzles/Puzzle202313.swift
@@ -8,7 +8,7 @@ struct Puzzle202313: Puzzle {
   // MARK: - Public methods
 
   func solve1() throws -> Any {
-    return -1
+    input.split(separator: "\n\n").reduce(0, { $0 + summarizePattern(String($1)) })
   }
 
   func solve2() throws -> Any {
@@ -16,4 +16,41 @@ struct Puzzle202313: Puzzle {
   }
 
   // MARK: - Private methods
+
+  private func findIndex(in pattern: String) -> Int? {
+    let lines = pattern.lines
+
+    outer: for i in 0..<lines.count {
+      let lastIndex = lines.count - 1
+      guard i < lastIndex else { break }
+
+      for j in 1...max(1, min(i + 1, lastIndex - i)) where lines[i - j + 1] != lines[i + j] {
+        continue outer
+      }
+      return i + 1
+    }
+    return nil
+  }
+
+  private func rotatePattern(_ pattern: String) -> String {
+    let lines = pattern.lines
+    var output = ""
+
+    for i in 0..<lines[0].count {
+      for j in (0..<lines.count).reversed() {
+        output += String(lines[j][i])
+      }
+      if i < lines[0].count - 1 {
+        output += "\n"
+      }
+    }
+    return output
+  }
+
+  private func summarizePattern(_ pattern: String) -> Int {
+    if let rowPattern = findIndex(in: pattern) {
+      return rowPattern * 100
+    }
+    return findIndex(in: rotatePattern(pattern)) ?? 0
+  }
 }
--- 2023/Tests/AdventOfCodeTests/Puzzle202313Tests.swift
+++ 2023/Tests/AdventOfCodeTests/Puzzle202313Tests.swift
@@ -4,11 +4,26 @@ import XCTest
 
 final class Puzzle202313Tests: XCTestCase {
   let input = """
+    #.##..##.
+    ..#.##.#.
+    ##......#
+    ##......#
+    ..#.##.#.
+    ..##..##.
+    #.#.##.#.
+
+    #...##..#
+    #....#..#
+    ..##..###
+    #####.##.
+    #####.##.
+    ..##..###
+    #....#..#
     """
 
   func testSolve1() throws {
     let sut = Puzzle202313(input: input)
-    XCTAssertEqual(try sut.solve1() as? Int, -1)
+    XCTAssertEqual(try sut.solve1() as? Int, 405)
   }
 
   func testSolve2() throws {
