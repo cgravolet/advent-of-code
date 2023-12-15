cc144f389e608281b19dcd021ff87aa526240540
cgravolet <cgravolet@gmail.com>
2023-12-15 09:08:20 -0500

    Solve - Day 15, part 1

--- 2023/Sources/AdventOfCode/Puzzles/Puzzle202315.swift
+++ 2023/Sources/AdventOfCode/Puzzles/Puzzle202315.swift
@@ -2,17 +2,22 @@ import Algorithms
 import Collections
 import Foundation
 
-private enum TiltDirection: CaseIterable {
-  case north, west, south, east
-}
-
 struct Puzzle202315: Puzzle {
   let input: String
 
   // MARK: - Public methods
 
   func solve1() throws -> Any {
-    return -1
+    input.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: ",")
+      .reduce(0, { value, step in
+        var cur = 0
+        for char in String(step) {
+          if let asciiValue = char.asciiValue {
+            cur = (cur + Int(asciiValue)) * 17 % 256
+          }
+        }
+        return value + cur
+      })
   }
 
   func solve2() throws -> Any {
--- 2023/Tests/AdventOfCodeTests/Puzzle202315Tests.swift
+++ 2023/Tests/AdventOfCodeTests/Puzzle202315Tests.swift
@@ -3,12 +3,11 @@ import XCTest
 @testable import AdventOfCode
 
 final class Puzzle202315Tests: XCTestCase {
-  let input = """
-    """
+  let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
 
   func testSolve1() throws {
     let sut = Puzzle202315(input: input)
-    XCTAssertEqual(try sut.solve1() as? Int, -1)
+    XCTAssertEqual(try sut.solve1() as? Int, 1320)
   }
 
   func testSolve2() throws {
