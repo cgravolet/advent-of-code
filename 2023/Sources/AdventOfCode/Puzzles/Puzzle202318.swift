2590ba9ffa7532c01c0cefdc85dd2abce2fa8e9e
cgravolet <cgravolet@gmail.com>
2023-12-18 19:41:38 -0500

    Solve - Day 18, part 1

--- 2023/Sources/AdventOfCode/Puzzles/Puzzle202318.swift
+++ 2023/Sources/AdventOfCode/Puzzles/Puzzle202318.swift
@@ -3,12 +3,36 @@ import Collections
 import Foundation
 
 struct Puzzle202318: Puzzle {
-  let input: String
+  let input: [String]
+
+  init(input: String) {
+    self.input = input.lines
+  }
 
   // MARK: - Public methods
 
   func solve1() throws -> Any {
-    return -1
+    var boundary = 0
+    let points =
+      input
+      .compactMap(parseLine)
+      .reduce(
+        into: [Coord2D.zero],
+        { points, input in
+          guard let direction = direction(for: input.0) else { return }
+          boundary += input.1
+          points.append(
+            points.last!
+              + (0..<input.1).reduce(into: Coord2D.zero, { val, _ in val += direction.coord }))
+        })
+    let area =
+      abs(
+        (0..<points.count - 2).reduce(0, { points[$1].x * points[$1 + 1].y + $0 })
+          - (0..<points.count - 2).reduce(0, { points[$1].y * points[$1 + 1].x + $0 })
+      ) / 2
+    let interior = area - (boundary / 2) + 1
+
+    return boundary + interior
   }
 
   func solve2() throws -> Any {
@@ -16,4 +40,24 @@ struct Puzzle202318: Puzzle {
   }
 
   // MARK: - Private methods
+
+  private func direction(for char: Character) -> CardinalDirection? {
+    switch char {
+    case "U": return .north
+    case "D": return .south
+    case "L": return .west
+    case "R": return .east
+    default: return nil
+    }
+  }
+
+  /// Parses a line from the given input (i.e. "R 6 (#70c710)")
+  private func parseLine(_ input: String) -> (Character, Int, String)? {
+    guard let match = input.firstMatch(of: /([UDLR])\s([0-9]+)\s\(#([A-Za-z0-9]{6})\)/),
+      let count = Int(match.output.2)
+    else { return nil }
+    let char = String(match.output.1)[0]
+    let hex = String(match.output.3)
+    return (char, count, hex)
+  }
 }
--- 2023/Tests/AdventOfCodeTests/Puzzle202318Tests.swift
+++ 2023/Tests/AdventOfCodeTests/Puzzle202318Tests.swift
@@ -4,11 +4,25 @@ import XCTest
 
 final class Puzzle202318Tests: XCTestCase {
   let input = """
+    R 6 (#70c710)
+    D 5 (#0dc571)
+    L 2 (#5713f0)
+    D 2 (#d2c081)
+    R 2 (#59c680)
+    D 2 (#411b91)
+    L 5 (#8ceee2)
+    U 2 (#caa173)
+    L 1 (#1b58a2)
+    U 2 (#caa171)
+    R 2 (#7807d2)
+    U 3 (#a77fa3)
+    L 2 (#015232)
+    U 2 (#7a21e3)
     """
 
   func testSolve1() throws {
     let sut = Puzzle202318(input: input)
-    XCTAssertEqual(try sut.solve1() as? Int, -1)
+    XCTAssertEqual(try sut.solve1() as? Int, 62)
   }
 
   func testSolve2() throws {
