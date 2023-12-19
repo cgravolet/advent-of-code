4cd36a7b9ed3ba2b8d71ec0c5f44d86ebe7e662c
cgravolet <cgravolet@gmail.com>
2023-12-19 00:39:50 -0500

    Cleanup

---
+++ 2023/Sources/AdventOfCode/Puzzles/Puzzle202319.swift
@@ -0,0 +1,19 @@
+import Algorithms
+import Collections
+import Foundation
+
+struct Puzzle202319: Puzzle {
+  let input: String
+
+  // MARK: - Public methods
+
+  func solve1() throws -> Any {
+    return -1
+  }
+
+  func solve2() throws -> Any {
+    return -1
+  }
+
+  // MARK: - Private methods
+}
